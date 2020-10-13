2622820b1a04e0da7ba857e9761040b4ec6a6f4b
agarciamorales23455 <agarciamorales23455@gmail.com>
2020-10-13 21:58:54 +0200

    documented main.py better

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ print("/nHello World")
 
 print(pd.DataFrame.__doc__)
 
+# created a simple DataFrame to test functionality
 df = pd.DataFrame(np.array([[1, 2, 3], 
                             [4, 5, 6], 
                             [7, 8, 9]]),
                             columns=['a', 'b', 'c'])
 
+# stored DataFrame in a local csv file
 df.to_csv("outputs/empty.csv", index=False)
 
