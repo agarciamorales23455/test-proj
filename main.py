9de91e40eeed0337a61ea7a0e2dbe67c1be833a3
agarciamorales23455 <agarciamorales23455@gmail.com>
2020-10-13 21:47:56 +0200

    added random df

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import numpy as np
 
 print("/nHello World")
 
-df = pd.DataFrame()
+print(pd.DataFrame.__doc__)
+
+df = pd.DataFrame(np.array([[1, 2, 3], 
+                            [4, 5, 6], 
+                            [7, 8, 9]]),
+                            columns=['a', 'b', 'c'])
 
 df.to_csv("outputs/empty.csv", index=False)
+
